60914e167f87aa810a56444970d9432e6a92c726
joshuaboe <jboe@calpoly.edu>
2018-12-03 16:44:55 -0800

    Updated History.py (was empty)

---
+++ src/Model/History.py
@@ -0,0 +1,10 @@
+#History includes songs listened to and the number of listens.
+#We need a way for dates to be mapped to songs for trend filtering reasons.
+#Maybe we will have another object that contains a song and the date it was played (listening_instance below)?
+
+class History:
+    def __init__(self):
+        pass
+        
+    def addSong(listening_instance):
+        #add song to user's history
