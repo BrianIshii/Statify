4932199bb579869e44a70ec53b80c697f709e499
jacksonmcdaniel <jackson.mcdaniel78@gmail.com>
2018-12-04 16:56:22 -0800

    Add .gitignore. Add script to start Django app. Add route to home page.

--- Statify/urls.py
+++ Statify/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('statifyapp.urls')),
     path('about/', include('statifyapp.urls')),
     path('admin/', admin.site.urls),
 ]
--- statifyapp/views.py
+++ statifyapp/views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 
 
 def index(request):
+    return HttpResponse("Hello, world. You're at the statify home page.")
+
+def about(request):
     return HttpResponse("Hello, world. You're at the statify about page.")
